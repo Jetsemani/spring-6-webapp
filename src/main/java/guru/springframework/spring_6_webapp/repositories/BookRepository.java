7b8f9d5ba5a8828a99a4e1afb0dce71e1f65f2bd
Jetse <jetsemani81@gmail.com>
2026-03-05 17:53:15 +0100

    create-books-service

--- src/main/java/guru/springframework/spring_6_webapp/repositories/BookRepository.java
+++ src/main/java/guru/springframework/spring_6_webapp/repositories/BookRepository.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring_6_webapp.domain.repositories;
+package guru.springframework.spring_6_webapp.repositories;
 
 import guru.springframework.spring_6_webapp.domain.Book;
 import org.springframework.data.repository.CrudRepository;
