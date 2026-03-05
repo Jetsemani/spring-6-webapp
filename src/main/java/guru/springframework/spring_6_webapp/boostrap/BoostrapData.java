6f131b82b0b9c616fbef76cd6fb4eb3019e3d524
Jetse <jetsemani81@gmail.com>
2026-03-05 17:06:32 +0100

    config database

--- src/main/java/guru/springframework/spring_6_webapp/boostrap/BoostrapData.java
+++ src/main/java/guru/springframework/spring_6_webapp/boostrap/BoostrapData.java
@@ -16,7 +16,8 @@ public class BoostrapData implements CommandLineRunner {
     private final BookRepository bookRepository;
     private final PublisherRepository publisherRepository;
 
-    public BoostrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+    public BoostrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
         this.publisherRepository = publisherRepository;
@@ -26,7 +27,7 @@ public class BoostrapData implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Author eric = new Author();
         eric.setFirstName("Eric");
-        eric.setLastName("Torres");
+        eric.setLastName("Evans");
 
         Book ddd = new Book();
         ddd.setTitle("Domain Driven Design");
@@ -40,49 +41,36 @@ public class BoostrapData implements CommandLineRunner {
         rod.setLastName("Johnson");
 
         Book noEJB = new Book();
-        noEJB.setTitle("J2EE development without EJB");
+        noEJB.setTitle("J2EE Development without EJB");
         noEJB.setIsbn("54757585");
 
         Author rodSaved = authorRepository.save(rod);
         Book noEJBSaved = bookRepository.save(noEJB);
 
-        Publisher publi1 = new Publisher();
-        publi1.setPublisherName("Editorial 1");
-        publi1.setAddress("Calle Mayor 15");
-        publi1.setCity("Madrid");
-        publi1.setState("Madrid");
-        publi1.setZip(28001);
-        Publisher publi1Saved = publisherRepository.save(publi1);
-
-        Publisher publi2 = new Publisher();
-        publi2.setPublisherName("Editorial 2");
-        publi2.setAddress("Calle Menor 15");
-        publi2.setCity("Toledo");
-        publi2.setState("Toledo");
-        publi2.setZip(95472);
-        Publisher publi2Saved = publisherRepository.save(publi2);
-
-        dddSaved.setPublisher(publi1Saved);
-        noEJBSaved.setPublisher(publi2Saved);
-
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
+        dddSaved.getAuthors().add(ericSaved);
+        noEJBSaved.getAuthors().add(rodSaved);
 
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("My Publisher");
+        publisher.setAddress("123 Main");
+        Publisher savedPublisher = publisherRepository.save(publisher);
+
+        dddSaved.setPublisher(savedPublisher);
+        noEJBSaved.setPublisher(savedPublisher);
 
         authorRepository.save(ericSaved);
         authorRepository.save(rodSaved);
-        publisherRepository.save(publi1Saved);
-        publisherRepository.save(publi2Saved);
+        bookRepository.save(dddSaved);
+        bookRepository.save(noEJBSaved);
 
         System.out.println("In Bootstrap");
-        System.out.println("Author Count:" + authorRepository.count());
-        System.out.println("Book Count:" + bookRepository.count());
-        System.out.println("Publisher Count:" + publisherRepository.count());
-        System.out.println(publi1);
+        System.out.println("Author Count: " + authorRepository.count());
+        System.out.println("Book Count: " + bookRepository.count());
 
 
 
-
-
+        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
